2df8f84070afff5e6220216e1316eaf5b94c94b9
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2022-11-15 03:10:22 -0500

    evaluacion 1 - inicial

---
+++ Evaluacion1/athlete_events.py
@@ -0,0 +1,34 @@
+# IMPORTANDO LIBRERIAS
+import math
+import pandas as pd
+
+# LEYENDA LA DATA
+df = pd.read_csv('athlete_events.csv')
+
+# INGRESO POR TECLADO
+sports = df.Sport.unique()
+
+def validacion(texto):
+    while True:
+        sport = input(texto).strip()
+        return sport if sport in sports else print("Ingrese un deporte valido")
+
+sportA = validacion("Ingrese un deporte: ")
+sportB = validacion("Ingrese otro deporte: ")
+
+
+# AMBOS DEPORTES PRESENTES
+listA = df.Year[df.Sport == sportA].unique()
+listB = df.Year[df.Sport == sportB].unique()
+
+listAB = sorted(list(set(listA) & set(listB)))
+
+# DETERMINAR ALTURAS
+for year in listAB:
+    
+    auxA = df.Height[(df.Year == year) & (df.Sport == 'Judo')].dropna()
+    auxB = df.Height[(df.Year == year) & (df.Sport == 'Football')].dropna()
+
+    result = (auxA.mean() - auxB.mean()) / math.sqrt((auxA.var() / auxA.count()) + (auxB.var() / auxB.count()))
+
+    print("Año " + str(year) + " D: " + ("Hay diferencia significativa" if abs(result) > 1.96 else "No hay diferencia significativa"))
---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align ="center" > Evaluaciones – Analista Data Science </h1>
+
+<h4 align ="center"> Evaluación técnica</h4>
+
+<br/>
+<p align="center">
+    <a href="https://github.com/ebercalderon/">
+      <img src="https://img.shields.io/badge/Evaluacion-1-blue" /></a> &#xa0;
+    <a href="https://github.com/ebercalderon/">
+      <img src="https://img.shields.io/badge/Evaluacion-2-blueviolet" /></a>
+</p>
+<br/>
+
+  * [Configuraciones](#configuraciones)
+  * [Características principales](#características-principales)
+  * [Tecnologías utilizadas](#tecnologías-utilizadas)
+  * [Screenshots](#screenshots)
+  * [Autor](#autor)
+
+
+## Configuraciones
+
+- Próximamente
+
+
+##  Características principales
+
+- Coming soon
+
+
+##  Tecnologías utilizadas
+
+- Próximamente
+
+ 
+##  Screenshots
+
+- Próximamente
+
+
+## Autor
+
+- Github: [@Eber](https://github.com/ebercalderon)
